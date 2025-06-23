9732cfb498f3e31563933beb60fff5a6c0337331
Dilkhush saini <sainidilkhush019@gmail.com>
2025-06-23 22:41:42 +0530

    remoing node_modules

--- MongoDb/app.js
+++ MongoDb/app.js
@@ -1,7 +1,18 @@
-const express = require("express")
+const express = require("express");
+const router = require("./routes/userRoutes");
 const app = express();
-const PORT = 3000;
+require("dotenv").config();
+const PORT = process.env.PORT || 3333;
+app.use(express.json());
 
-app.listen(PORT,() => {
-    console.log(`server started on port${PORT}`)
-})
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => console.log("MongoDB Connected::"))
+  .catch((err) => console.log("Not connected:", err));
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
---
+++ MongoDb/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const User = require("../models/userModel");
+// Creating user-----
+
+router.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+module.exports = router;
